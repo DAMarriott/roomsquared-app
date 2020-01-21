c45d3040ce79cabfa2f905b7eccd29761ea92c17
DA Marriott <DMarriott93@gmail.com>
2020-01-20 16:14:21 -0800

    Updates

---
+++ precache-manifest.685dc170786c6f815fbd1a7e7f5c6951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0693da98312e1017f819d44d8f303d49",
+    "url": "/roomsquared-app/index.html"
+  },
+  {
+    "revision": "2e08cccd42d36fafede4",
+    "url": "/roomsquared-app/static/css/main.ccbb44d2.chunk.css"
+  },
+  {
+    "revision": "47479bc019c42e5eaca8",
+    "url": "/roomsquared-app/static/js/2.6b445b4b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/roomsquared-app/static/js/2.6b445b4b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e08cccd42d36fafede4",
+    "url": "/roomsquared-app/static/js/main.45d567b7.chunk.js"
+  },
+  {
+    "revision": "220d3bd966bd3b82522e",
+    "url": "/roomsquared-app/static/js/runtime-main.2543fcd4.js"
+  }
+]);
